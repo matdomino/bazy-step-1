ed427fc60ed3c9d9343f02f991c19e05be697014
matdomino <mdomino60@gmail.com>
2023-12-08 00:42:39 +0100

    Utworzenie serwera express, dodanie laczenia z baza danych, utworzenie endpointu /products

--- src/serverExpress.js
+++ src/serverExpress.js
@@ -1,11 +1,46 @@
 'use strict';
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import { MongoClient, ObjectId } from 'mongodb';
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 
-const dbUrl = ''
+const dbUrl = 'mongodb://localhost:27017/';
+const dbName = 'warehouseDB';
+
+async function connect() {
+    try {
+        const client = new MongoClient(dbUrl);
+        await client.connect();
+        console.log('Pomyślnie połączono z bazą danych!');
+
+        const db = client.db(dbName);
+        const productsCollection = db.collection('products')
+
+        app.get('/products', async (req, res) => {
+            try {
+                const { sortBy, sortOrder } = req.body;
+                const sortOptions = {};
+                if (sortBy) {
+                    sortOptions[sortBy] = sortOrder === 'desc' ? -1 : 1;
+                }
+                const products = await productsCollection.find().sort(sortOptions).toArray();
+                res.json(products);
+            } catch (err) {
+                console.error(err);
+            }
+        });
+
+        app.listen(port, () => {
+            console.log(`Serwer działa na porcie: ${port}`);
+        });
+
+    } catch (err) {
+        console.error('Wystąpił błąd podczas łączenia z serwerem.', err);
+    }
+}
+
+connect();
