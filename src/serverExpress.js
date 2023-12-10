04a3cfc20d6c8bca16e5bbdf615faad6980e747a
matdomino <mdomino60@gmail.com>
2023-12-10 20:22:31 +0100

    Poprawka dodania produktu, implementacja usuniecia i raportu magazynu

--- src/serverExpress.js
+++ src/serverExpress.js
@@ -31,19 +31,26 @@ async function connect() {
                 res.json(products);
             } catch (err) {
                 console.error(err);
+                return res.send(err);
             }
         });
 
         app.post('/products', async (req, res) => {
             try {
                 const { name, price, description, quantity, unit } = req.body;
-                const product = {
-                    "name": 0,
-                    "price": 0,
-                    "description": 0,
-                    "quantity": 0,
-                    "unit": 0
+
+                const existingProduct = await productsCollection.findOne({ "name": name });
+                if (existingProduct) {
+                    return res.json({ error: 'Produkt o tej nazwie już istnieje w bazie danych.' });
                 }
+
+                const product = {
+                    "name": name,
+                    "price": price,
+                    "description": description,
+                    "quantity": quantity,
+                    "unit": unit
+                };
             
                 if (
                     typeof name !== 'string' ||
@@ -53,16 +60,100 @@ async function connect() {
                     !Number.isInteger(quantity) ||
                     typeof unit !== 'string'
                 ) {
-                    res.json({ error: 'Błędne dane' });
-                    return;
+                    return res.json({ error: 'Błędne dane.' });
                 }
 
                 const result = await productsCollection.insertOne(product);
-                res.json(result);
+                if (result.insertedCount === 1) {
+                    return res.json({ success: 'Dodano produkt.' });
+                } else {
+                    return res.json({ error: 'Błąd. Nie udało się dodać produktu.' });
+                }
+
+            } catch (err) {
+                res.send(err);
+                console.error(err);
+            }
+        });
+
+        app.put('/products/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const { newName, newPrice, newDescription, newQuantity, newUnit } = req.body;
+
+                const existingProduct = await productsCollection.findOne({ "_id": ObjectId(id) });
+                if (!(existingProduct)) {
+                    res.json({ error: 'Brak produktu o takim id w bazie danych.' });
+                    return;
+                }
+
+                const updateData = {};
+                if (newName !== "") updateData.name = newName;
+                if (newPrice !== "") updateData.price = newPrice;
+                if (newDescription !== "") updateData.description = newDescription;
+                if (newQuantity !== "") updateData.quantity = newQuantity;
+                if (newUnit !== "") updateData.unit = newUnit;
+
+                const result = await productsCollection.updateOne(
+                    { "_id": ObjectId(id) },
+                    {
+                        $set: updateData
+                    }
+                );
+
+                if (result.modifiedCount === 1) {
+                    return res.json({ success: 'Produkt został zaktualizowany.' });
+                } else {
+                    return res.json({ error: 'Nie udało się zaktualizować produktu.' });
+                }
+
+            } catch (err) {
+                res.send(err);
+                console.error(err);
+            }
+        });
+
+        app.delete('/products/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const product = await productsCollection.findOne({ "_id": ObjectId(id) });
+                if (!(product)) {
+                    res.json({ error: 'Brak produktu o takim id w bazie danych.' });
+                    return;
+                }
+
+                const result = await productsCollection.deleteOne(
+                    { "_id": ObjectId(id) }
+                );
+
+                if (result.deletedCount === 1) {
+                    return res.json({ success: 'Usunięto produkt.' });
+                } else {
+                    return res.json({ error: 'Nie udało się usunąć produktu.' });
+                }
 
             } catch (err) {
                 console.error(err);
-                res.send('Wystąpił błąd serwera lub podałeś za mało bądź za dużo danych');
+                return res.send(err);
+            }
+        });
+
+        app.get('/report', async (req, res) => {
+            try {
+                const products = await productsCollection.find().toArray();
+        
+                const report = products.map(product => {
+                    return {
+                        name: product.name,
+                        quantity: product.quantity.toString() + ' ' + product.unit,
+                        totalValue: product.quantity * product.price
+                    };
+                });
+        
+                res.json(report);
+            } catch (err) {
+                console.error(err);
+                res.send(err);
             }
         });
 
@@ -71,7 +162,7 @@ async function connect() {
         });
 
     } catch (err) {
-        console.error('Wystąpił błąd podczas łączenia z serwerem.', err);
+        console.error('Wystąpił błąd podczas łączenia z bazą.', err);
     }
 }
 
